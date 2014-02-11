6b6c94b284099fc346c43bab76a1ed01ec2ddbde
Todor Paskalev <todor.paskalev@gmail.com>
2014-02-11 15:52:33 +0200

    #164078 Add Border Layout Item into toolbox. Add config json. Add case into the layout component plugin

--- src/plugins/layout-component-plugin.js
+++ src/plugins/layout-component-plugin.js
@@ -14,7 +14,14 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 			if (descriptor.type === "flowLayout") {
 				return "<div style=\"border: 1px solid #eee; height: 310px; width: 650px;\">\n\t\t<ul id=\"" + descriptor.id + "\"></ul>\n\t</div>";
 			} else if (descriptor.type === "borderLayout") {
-				
+				return "<div style=\"height: 300px; width: 600px;\" id=\"" + descriptor.id + "\"><div class=\"left\">" +
+			"<h3>LEFT AREA</h3><p>First paragraph</p><p>Second paragraph</p><p>Third paragraph</p><p>Fourth paragraph</p></div><div class=\"right\">" +
+			"<h3>RIGHT AREA</h3>" +
+			"<p>Aliquam ut tellus tristique massa gravida accumsan. Vivamus sodales, purus eget vehicula aliquam, arcu ipsum hendrerit mauris, ac faucibus massa turpis eget nibh. Etiam blandit sodales semper. Curabitur enim nunc, molestie ac aliquam eu, imperdiet tincidunt orci. Proin pulvinar vehicula lacus, ut lacinia tortor molestie vitae. Nunc ut augue id erat feugiat feugiat non sit amet erat. Suspendisse ultrices risus dapibus erat laoreet accumsan. Phasellus rutrum, lectus quis tempus blandit, libero lectus dignissim ligula, in semper orci nibh ut diam.</p></div>" +
+			"<div class=\"header\">HEADER - some text here</div>" +
+			"<div class=\"footer\">FOOTER - some text here </div>" +
+			"<div class=\"center\">" +
+			"<h3>CENTER AREA</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget porta urna. Ut gravida mi at ligula commodo feugiat vehicula lacus tincidunt. Proin lobortis magna sed lacus malesuada commodo fermentum felis auctor. Sed quis nulla quis tellus facilisis malesuada. Mauris aliquam neque consequat mi blandit in luctus magna rutrum. Fusce sit amet ipsum magna. Vivamus porttitor arcu vitae eros molestie et sagittis dolor cursus. Quisque ultrices feugiat risus, vitae molestie felis interdum ac. Suspendisse pellentesque magna nec est commodo porttitor. </p><p>Nunc lacinia ligula a nisl porta sed facilisis mauris facilisis. Sed a ante turpis, eget semper odio. Curabitur facilisis faucibus iaculis. Ut fermentum pretium ultrices. Etiam sem dui, adipiscing sed tempus et, adipiscing eget erat. Vestibulum nec eros magna.</p><p>Praesent tristique arcu eget ligula pulvinar mollis. Maecenas at elit at justo posuere gravida id eu enim. Duis imperdiet lectus nec augue sollicitudin hendrerit. Suspendisse consectetur, lorem nec eleifend rutrum, eros metus sodales libero, et rutrum diam augue et lectus. Integer sed est vitae risus dignissim condimentum ullamcorper at massa. Donec a orci nisl, eget ullamcorper augue. Quisque sagittis fringilla tortor. Vivamus ligula velit, aliquam ut pellentesque quis, fringilla sit amet tortor.</p></div></div>";
 			} else {
 				return "<div id=\"" + descriptor.id + "\"></div>";
 			}
@@ -31,7 +38,14 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 			if (descriptor.type === "flowLayout") {
 				return {codeString: "\t" + extraIndentStr + "<div style=\"border: 1px solid #eee; height: 160px; width: 700px;\">\n\t\t\t<ul id=\"" + descriptor.id + "\"></ul>\n\t\t</div>\n",lineCount: 3};
 			} else if (descriptor.type === "borderLayout") {
-				
+				return {codeString: "\t" + extraIndentStr + "<div style=\"height: 300px; width: 650px;\" id=\"" + descriptor.id + "\">\n\t\t\t<div class=\"left\">" +
+			"\n\t\t\t\t<h3>LEFT AREA</h3>\n\t\t\t\t<p>First paragraph</p>\n\t\t\t\t<p>Second paragraph</p>\n\t\t\t\t<p>Third paragraph</p>\n\t\t\t\t<p>Fourth paragraph</p>\n\t\t\t</div>\n\t\t\t<div class=\"right\">" +
+			"\n\t\t\t\t<h3>RIGHT AREA</h3>" +
+			"\n\t\t\t\t<p>Aliquam ut tellus tristique massa gravida accumsan. Vivamus sodales, purus eget vehicula aliquam, arcu ipsum hendrerit mauris, ac faucibus massa turpis eget nibh. Etiam blandit sodales semper. Curabitur enim nunc, molestie ac aliquam eu, imperdiet tincidunt orci. Proin pulvinar vehicula lacus, ut lacinia tortor molestie vitae. Nunc ut augue id erat feugiat feugiat non sit amet erat. Suspendisse ultrices risus dapibus erat laoreet accumsan. Phasellus rutrum, lectus quis tempus blandit, libero lectus dignissim ligula, in semper orci nibh ut diam.</p>\n\t\t\t</div>" +
+			"\n\t\t\t<div class=\"header\">HEADER - some text here</div>" +
+			"\n\t\t\t<div class=\"footer\">FOOTER - some text here </div>" +
+			"\n\t\t\t<div class=\"center\">" +
+			"\n\t\t\t\t<h3>CENTER AREA</h3>\n\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget porta urna. Ut gravida mi at ligula commodo feugiat vehicula lacus tincidunt. Proin lobortis magna sed lacus malesuada commodo fermentum felis auctor. Sed quis nulla quis tellus facilisis malesuada. Mauris aliquam neque consequat mi blandit in luctus magna rutrum. Fusce sit amet ipsum magna. Vivamus porttitor arcu vitae eros molestie et sagittis dolor cursus. Quisque ultrices feugiat risus, vitae molestie felis interdum ac. Suspendisse pellentesque magna nec est commodo porttitor. </p>\n\t\t\t\t<p>Nunc lacinia ligula a nisl porta sed facilisis mauris facilisis. Sed a ante turpis, eget semper odio. Curabitur facilisis faucibus iaculis. Ut fermentum pretium ultrices. Etiam sem dui, adipiscing sed tempus et, adipiscing eget erat. Vestibulum nec eros magna.</p>\n\t\t\t\t<p>Praesent tristique arcu eget ligula pulvinar mollis. Maecenas at elit at justo posuere gravida id eu enim. Duis imperdiet lectus nec augue sollicitudin hendrerit. Suspendisse consectetur, lorem nec eleifend rutrum, eros metus sodales libero, et rutrum diam augue et lectus. Integer sed est vitae risus dignissim condimentum ullamcorper at massa. Donec a orci nisl, eget ullamcorper augue. Quisque sagittis fringilla tortor. Vivamus ligula velit, aliquam ut pellentesque quis, fringilla sit amet tortor.</p>\n\t\t\t</div>\n\t\t</div>\n",lineCount: 21};
 			} else {
 				return {codeString: "\t" + extraIndentStr + "<div id=\"" + descriptor.id + "\"></div>\n", lineCount: 1};
 			}			
