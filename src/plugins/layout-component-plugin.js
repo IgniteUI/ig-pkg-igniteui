b126d37a7b31329aaececd60cc29ae9f84999579
Todor Paskalev <todor.paskalev@gmail.com>
2014-02-12 18:27:18 +0200

    #163592 Add grid layout into the toolbox. add config json. add drag icon. add default configuration

--- src/plugins/layout-component-plugin.js
+++ src/plugins/layout-component-plugin.js
@@ -38,7 +38,7 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 				}
 			}
 			if (descriptor.type === "flowLayout") {
-				return {codeString: "\t" + extraIndentStr + "<div style=\"border: 1px solid #eee; height: 160px; width: 700px;\">\n\t\t\t<ul id=\"" + descriptor.id + "\"></ul>\n\t\t</div>\n",lineCount: 3};
+				return {codeString: "\t" + extraIndentStr + "<div style=\"border: 1px solid #eee; height: 160px; width: 700px;\">\n\t\t\t<ul id=\"" + descriptor.id + "\"></ul>\n\t\t</div>\n", lineCount: 3};
 			} else if (descriptor.type === "borderLayout") {
 				return {codeString: "\t" + extraIndentStr + "<div style=\"height: 300px; width: 650px;\" id=\"" + descriptor.id + "\">\n\t\t\t<div class=\"left\">" +
 			"\n\t\t\t\t<h3>LEFT AREA</h3>\n\t\t\t\t<p>First paragraph</p>\n\t\t\t\t<p>Second paragraph</p>\n\t\t\t\t<p>Third paragraph</p>\n\t\t\t\t<p>Fourth paragraph</p>\n\t\t\t</div>\n\t\t\t<div class=\"right\">" +
@@ -47,16 +47,35 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 			"\n\t\t\t<div class=\"header\">HEADER - some text here</div>" +
 			"\n\t\t\t<div class=\"footer\">FOOTER - some text here </div>" +
 			"\n\t\t\t<div class=\"center\">" +
-			"\n\t\t\t\t<h3>CENTER AREA</h3>\n\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget porta urna. Ut gravida mi at ligula commodo feugiat vehicula lacus tincidunt. Proin lobortis magna sed lacus malesuada commodo fermentum felis auctor. Sed quis nulla quis tellus facilisis malesuada. Mauris aliquam neque consequat mi blandit in luctus magna rutrum. Fusce sit amet ipsum magna. Vivamus porttitor arcu vitae eros molestie et sagittis dolor cursus. Quisque ultrices feugiat risus, vitae molestie felis interdum ac. Suspendisse pellentesque magna nec est commodo porttitor. </p>\n\t\t\t\t<p>Nunc lacinia ligula a nisl porta sed facilisis mauris facilisis. Sed a ante turpis, eget semper odio. Curabitur facilisis faucibus iaculis. Ut fermentum pretium ultrices. Etiam sem dui, adipiscing sed tempus et, adipiscing eget erat. Vestibulum nec eros magna.</p>\n\t\t\t\t<p>Praesent tristique arcu eget ligula pulvinar mollis. Maecenas at elit at justo posuere gravida id eu enim. Duis imperdiet lectus nec augue sollicitudin hendrerit. Suspendisse consectetur, lorem nec eleifend rutrum, eros metus sodales libero, et rutrum diam augue et lectus. Integer sed est vitae risus dignissim condimentum ullamcorper at massa. Donec a orci nisl, eget ullamcorper augue. Quisque sagittis fringilla tortor. Vivamus ligula velit, aliquam ut pellentesque quis, fringilla sit amet tortor.</p>\n\t\t\t</div>\n\t\t</div>\n",lineCount: 21};
+			"\n\t\t\t\t<h3>CENTER AREA</h3>\n\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget porta urna. Ut gravida mi at ligula commodo feugiat vehicula lacus tincidunt. Proin lobortis magna sed lacus malesuada commodo fermentum felis auctor. Sed quis nulla quis tellus facilisis malesuada. Mauris aliquam neque consequat mi blandit in luctus magna rutrum. Fusce sit amet ipsum magna. Vivamus porttitor arcu vitae eros molestie et sagittis dolor cursus. Quisque ultrices feugiat risus, vitae molestie felis interdum ac. Suspendisse pellentesque magna nec est commodo porttitor. </p>\n\t\t\t\t<p>Nunc lacinia ligula a nisl porta sed facilisis mauris facilisis. Sed a ante turpis, eget semper odio. Curabitur facilisis faucibus iaculis. Ut fermentum pretium ultrices. Etiam sem dui, adipiscing sed tempus et, adipiscing eget erat. Vestibulum nec eros magna.</p>\n\t\t\t\t<p>Praesent tristique arcu eget ligula pulvinar mollis. Maecenas at elit at justo posuere gravida id eu enim. Duis imperdiet lectus nec augue sollicitudin hendrerit. Suspendisse consectetur, lorem nec eleifend rutrum, eros metus sodales libero, et rutrum diam augue et lectus. Integer sed est vitae risus dignissim condimentum ullamcorper at massa. Donec a orci nisl, eget ullamcorper augue. Quisque sagittis fringilla tortor. Vivamus ligula velit, aliquam ut pellentesque quis, fringilla sit amet tortor.</p>\n\t\t\t</div>\n\t\t</div>\n", lineCount: 21};
 			} else if (descriptor.type === "columnLayout") {
 				return {codeString: "\t" + extraIndentStr + "<div id=\"layout\" class=\"ig-layout-col\">\n\t\t\t<div class=\"row\">\n\t\t\t\t<div class=\"col3\">\n\t\t\t\t\t<h3>Heading</h3>\n\t\t\t\t\t<p>Contents</p>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col3\">\n\t\t\t\t\t<h3>Heading</h3>\n\t\t\t\t\t<p>Contents</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<div class=\"row\">\n\t\t\t\t<div class=\"col12\">\n\t\t\t\t\t<div class=\"col7\">\n\t\t\t\t\t\t<h3>Heading 7 cols </h3>\n\t\t\t\t\t\t<p>Seven cols content</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<div class=\"row\">\n\t\t\t\t<div class=\"col2\">\n\t\t\t\t\t<h3>Heading 2 cols</h3>\n\t\t\t\t\t<p>Two cols content</p>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col5\">\n\t\t\t\t\t<h3>Heading 5 cols</h3>\n\t\t\t\t\t<p>Five cols Content</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n", lineCount: 1};
-				
 			} else {
 				return {codeString: "\t" + extraIndentStr + "<div id=\"" + descriptor.id + "\"></div>\n", lineCount: 1};
 			}			
 			//return this.evalTemplate("default.code.html", descriptor);
+		},
+		getCodeEditorScriptSnippet: function (descriptor) {
+			var snippet = this._super(descriptor);
+			if (descriptor.type === "gridLayout") {
+				var handler = "\t\t\t\t$(\"#" + descriptor.id +"\").on(\"iglayoutmanageritemrendered\", function (event, args) {\n\t\t\t\t\t args.item.append(\"<ul><li>colspan: \" + args.itemData.colSpan + \"</li><li>rowspan: \" + args.itemData.rowSpan + \"</li></ul></span>\");\n\t\t\t\t});\n";
+				snippet.codeString = handler + snippet.codeString; 
+				snippet.lineCount += 3;
+			}
+			return snippet;
+		},
+		initComponent: function (descriptor) {
+			var name = this._getWidgetName(descriptor.type);
+			if (descriptor.type === "gridLayout") {
+				window.frames[0].$(descriptor.placeholder).on("iglayoutmanageritemrendered", function (e, args) {
+					args.item.append("<ul><li>colspan: " + args.itemData.colSpan + "</li><li>rowspan: " + args.itemData.rowSpan + "</li></ul></span>");
+				});
+			}
+			if (descriptor.type !== "columnLayout" && window.frames[0].$(descriptor.placeholder)[name]) {
+				window.frames[0].$(descriptor.placeholder)[name](descriptor.options);
+			}
+
 		}
-		
 	});
 	return IgniteUIBorderLayoutPlugin;
 });
