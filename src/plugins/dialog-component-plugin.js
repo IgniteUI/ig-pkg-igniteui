c400570477e069d8ffc2e43b12a5c13577bdcfbb
kdinev <kdinev@mail.bw.edu>
2014-02-19 18:28:37 +0200

    #164533 Adding a designer attribute to recognize the DOM elements inside the dialog that need to be picked up as a DOM link.

--- src/plugins/dialog-component-plugin.js
+++ src/plugins/dialog-component-plugin.js
@@ -12,7 +12,7 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 		getCodeEditorMarkupSnippet: function (descriptor) {
 			var snippet = {
 				codeString: descriptor.ide._tabStr(descriptor.extraIndent + 1) + "<div id=\"" + descriptor.id + "\">\n" +
-					descriptor.ide._tabStr(descriptor.extraIndent + 2) + "<div></div>\n" +
+					descriptor.ide._tabStr(descriptor.extraIndent + 2) + "<div class='ui-igdialog-content'></div>\n" +
 					descriptor.ide._tabStr(descriptor.extraIndent + 1) + "</div>\n", 
 				lineCount: 3,
 				extraMarkers: [
@@ -20,6 +20,13 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 				]
 			};
 			return snippet;
+		},
+		initComponent: function (descriptor) {
+			var name = this._getWidgetName(descriptor.type);
+			if (window.frames[0].$(descriptor.placeholder)[name]) { 
+				window.frames[0].$(descriptor.placeholder)[name](descriptor.options);
+				window.frames[0].$(descriptor.placeholder).children('ui-igdialog-content').attr('data-design-visible-child', true);
+			}
 		}
 	});
 	return IgniteUIDialogPlugin;
