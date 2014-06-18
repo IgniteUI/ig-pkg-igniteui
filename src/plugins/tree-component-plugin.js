2f093f60383e0dbe1206e561dbc34ca3e694eadf
sdanailova <sdanailova@infragistics.com>
2014-06-18 17:18:54 +0300

    #172287 Removing getCodeEditorScriptSnippet from Tree plugin

--- src/plugins/tree-component-plugin.js
+++ src/plugins/tree-component-plugin.js
@@ -1,39 +1,5 @@
 define (["./_default-component-plugin"], function (DefaultPlugin) {
 	var IgniteUITreePlugin = IgniteUITreePlugin || DefaultPlugin.extend({
-		getCodeEditorScriptSnippet: function (descriptor) {
-			var code = "";
-			var lineCount = 8;
-			var orderedReturnProps = [];
-			var xtraMarkup = "\t\t\t\t\tbindings: {\n"
-					+ "\t\t\t\t\t\ttextKey: \"Name\",\n"
-					+ "\t\t\t\t\t\tvalueKey: \"ID\",\n"
-					+ "\t\t\t\t\t\tchildDataProperty: \"children\"\n"
-					+ "\t\t\t\t\t}";
-			code = "\t\t\t\t$(\"#" + descriptor.id + "\").igTree({\n" + xtraMarkup;
-			orderedReturnProps.push({
-				name: "bindings",
-				value: {
-					textKey: "Name",
-					valueKey: "ID",
-					childDataProperty: "children"
-				},
-				type: "object"
-			});
-			if (descriptor.data && window[descriptor.data]) {
-				code += ",\n\t\t\t\t\tdataSource: " + descriptor.data;
-				orderedReturnProps.push({
-					name: "dataSource",
-					value: descriptor.data,
-					type: "literal"
-				});
-			}
-			code += "\n\t\t\t\t});\n";
-			return {
-				codeString: code, 
-				lineCount: lineCount, 
-				orderedProps: orderedReturnProps
-			};
-		},
 		addExtraMarkers: function (descriptor) {
 			this._super(descriptor);
 			var bindings = this.settings.editor.find("bindings", {start: descriptor.marker.range.start});
