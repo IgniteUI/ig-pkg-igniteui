1cb68c89dba464aefbeaabfd7d5b0f7b4bbfc1e2
Angel Todorov <attodorov@gmail.com>
2014-06-20 14:23:37 +0300

    fix for #169426

--- src/plugins/_default-component-plugin.js
+++ src/plugins/_default-component-plugin.js
@@ -1280,6 +1280,11 @@ define (function (require, exports, module) {
 											}
 										}
 									}
+								} else if (node.type === "AssignmentExpression") {
+									//A.T. Bug #168894 - recognize "pasted" data source code
+									// get the str
+									var line = ide.session.getTextRange(new ide.RangeClass(node.loc.start.line + scriptRanges[i].start.row - 1, 0, node.loc.start.line + scriptRanges[i].start.row - 1, 1000));
+									
 								}
 							},
 							leave: function (node, parent) {
