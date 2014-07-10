7020c3902ae6c44aa464e656a4e8de65ea1ec4dd
kdinev <kdinev@mail.bw.edu>
2014-07-10 19:06:09 +0300

    #174356 Fixing function in the circleMarkerTemplate of the chart

--- src/plugins/charts-component-plugin.js
+++ src/plugins/charts-component-plugin.js
@@ -1,6 +1,13 @@
 define (["./_default-component-plugin"], function (DefaultPlugin) {
 	var IgniteUIChartsPlugin = IgniteUIChartsPlugin || DefaultPlugin.extend({
 		getPropValue: function (descriptor) {
+			if (descriptor.comp && descriptor.propName === "circleMarkerTemplate") {
+				var prop = descriptor.comp.options[descriptor.propName];
+				if (!prop) {
+					prop = this._super(descriptor);
+				}
+				return prop;
+			}
 			if (descriptor.propValue) {
 				var prop = descriptor.propValue;
 				if (!prop) {
