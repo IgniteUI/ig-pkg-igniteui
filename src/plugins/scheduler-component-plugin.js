b1a03fb6ad656f4ac33ae8036b0ca817136e8b19
Nikolay Alipiev <nikolay.alipiev@gmail.com>
2017-07-03 17:02:05 +0300

    Properly save and restore scheduler data source.

--- src/plugins/scheduler-component-plugin.js
+++ src/plugins/scheduler-component-plugin.js
@@ -6,6 +6,10 @@ define (["./_default-component-plugin"], function (DefaultPlugin) {
 			// Schedule data needs translated scheduler files and therefore it is placed in a differenct schedulerDataSource.js (all default datasource are in datasources.js file)
 			if (descriptor.data && window.frames[0][descriptor.data]) {
 				descriptor.options.dataSource = window.frames[0][descriptor.data];
+
+				// dsID is the name of the data source, needed to save and restore scheduler data source,
+				// otherwise the data source itself (because it is object with circular references) cannot be stringified. 
+				descriptor.options.dsID = descriptor.data;
 			}
 
 		    if (window.frames[0].$(descriptor.placeholder)[name]) {
